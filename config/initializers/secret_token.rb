aa6bb6bea466a0fa61d1dc9f488f79e2c2b6839a
sundar <sunarth@outlook.com>
2013-12-17 10:31:52 +0530

    created a rails app and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharespace::Application.config.secret_token = '9dbfe0e9ebdb175ef95586f09d92708a125867922ff45195e7e06dc76a0918be45697c4a327c4b9b5318cfa73eb673d79fe2644686394efc42e46d6e02c6bf39'
